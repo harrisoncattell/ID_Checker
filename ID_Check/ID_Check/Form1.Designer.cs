47ed4bce6be86f5d09ed004211962fb01cdb3114
harrisoncattell <15562670@students.lincoln.ac.uk>
2017-07-26 07:39:09 +0100

    Added Source Code

---
+++ ID_Check/ID_Check/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace ID_Check
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtDoB = new System.Windows.Forms.TextBox();
+            this.lbl_ID_Checker = new System.Windows.Forms.Label();
+            this.lbl_Date_Of_Birth = new System.Windows.Forms.Label();
+            this.lbl_Current_Date = new System.Windows.Forms.Label();
+            this.Current_Date_Label = new System.Windows.Forms.Label();
+            this.btn_Number_1 = new System.Windows.Forms.Button();
+            this.btn_Number_4 = new System.Windows.Forms.Button();
+            this.btn_Number_7 = new System.Windows.Forms.Button();
+            this.btn_Number_8 = new System.Windows.Forms.Button();
+            this.btn_Number_5 = new System.Windows.Forms.Button();
+            this.btn_Number_2 = new System.Windows.Forms.Button();
+            this.btn_Number_9 = new System.Windows.Forms.Button();
+            this.btn_Number_6 = new System.Windows.Forms.Button();
+            this.btn_Number_3 = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btn_Number_0 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtDoB
+            // 
+            this.txtDoB.Location = new System.Drawing.Point(282, 153);
+            this.txtDoB.Name = "txtDoB";
+            this.txtDoB.Size = new System.Drawing.Size(239, 22);
+            this.txtDoB.TabIndex = 1;
+            // 
+            // lbl_ID_Checker
+            // 
+            this.lbl_ID_Checker.AutoSize = true;
+            this.lbl_ID_Checker.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ID_Checker.Location = new System.Drawing.Point(57, 9);
+            this.lbl_ID_Checker.Name = "lbl_ID_Checker";
+            this.lbl_ID_Checker.Size = new System.Drawing.Size(431, 100);
+            this.lbl_ID_Checker.TabIndex = 2;
+            this.lbl_ID_Checker.Text = "ID Checker";
+            // 
+            // lbl_Date_Of_Birth
+            // 
+            this.lbl_Date_Of_Birth.AutoSize = true;
+            this.lbl_Date_Of_Birth.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Date_Of_Birth.Location = new System.Drawing.Point(12, 137);
+            this.lbl_Date_Of_Birth.Name = "lbl_Date_Of_Birth";
+            this.lbl_Date_Of_Birth.Size = new System.Drawing.Size(248, 48);
+            this.lbl_Date_Of_Birth.TabIndex = 3;
+            this.lbl_Date_Of_Birth.Text = "Date of Birth:";
+            // 
+            // lbl_Current_Date
+            // 
+            this.lbl_Current_Date.AutoSize = true;
+            this.lbl_Current_Date.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Current_Date.Location = new System.Drawing.Point(12, 237);
+            this.lbl_Current_Date.Name = "lbl_Current_Date";
+            this.lbl_Current_Date.Size = new System.Drawing.Size(252, 48);
+            this.lbl_Current_Date.TabIndex = 8;
+            this.lbl_Current_Date.Text = "Current Date:";
+            // 
+            // Current_Date_Label
+            // 
+            this.Current_Date_Label.AutoSize = true;
+            this.Current_Date_Label.Font = new System.Drawing.Font("Adobe Fan Heiti Std B", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Current_Date_Label.Location = new System.Drawing.Point(274, 237);
+            this.Current_Date_Label.Name = "Current_Date_Label";
+            this.Current_Date_Label.Size = new System.Drawing.Size(98, 48);
+            this.Current_Date_Label.TabIndex = 9;
+            this.Current_Date_Label.Text = "------";
+            // 
+            // btn_Number_1
+            // 
+            this.btn_Number_1.Location = new System.Drawing.Point(20, 318);
+            this.btn_Number_1.Name = "btn_Number_1";
+            this.btn_Number_1.Size = new System.Drawing.Size(109, 85);
+            this.btn_Number_1.TabIndex = 11;
+            this.btn_Number_1.Text = "1";
+            this.btn_Number_1.UseVisualStyleBackColor = true;
+            this.btn_Number_1.Click += new System.EventHandler(this.btn_Number_1_Click);
+            // 
+            // btn_Number_4
+            // 
+            this.btn_Number_4.Location = new System.Drawing.Point(20, 409);
+            this.btn_Number_4.Name = "btn_Number_4";
+            this.btn_Number_4.Size = new System.Drawing.Size(109, 85);
+            this.btn_Number_4.TabIndex = 12;
+            this.btn_Number_4.Text = "4";
+            this.btn_Number_4.UseVisualStyleBackColor = true;
+            this.btn_Number_4.Click += new System.EventHandler(this.btn_Number_4_Click);
+            // 
+            // btn_Number_7
+            // 
+            this.btn_Number_7.Location = new System.Drawing.Point(20, 500);
+            this.btn_Number_7.Name = "btn_Number_7";
+            this.btn_Number_7.Size = new System.Drawing.Size(109, 85);
+            this.btn_Number_7.TabIndex = 13;
+            this.btn_Number_7.Text = "7";
+            this.btn_Number_7.UseVisualStyleBackColor = true;
+            this.btn_Number_7.Click += new System.EventHandler(this.btn_Number_7_Click);
+            // 
+            // btn_Number_8
+            // 
+            this.btn_Number_8.Location = new System.Drawing.Point(151, 500);
+            this.btn_Number_8.Name = "btn_Number_8";
+            this.btn_Number_8.Size = new System.Drawing.Size(109, 85);
+            this.btn_Number_8.TabIndex = 16;
+            this.btn_Number_8.Text = "8";
+            this.btn_Number_8.UseVisualStyleBackColor = true;
+            this.btn_Number_8.Click += new System.EventHandler(this.btn_Number_8_Click);
+            // 
+            // btn_Number_5
+            // 
+            this.btn_Number_5.Location = new System.Drawing.Point(151, 409);
+            this.btn_Number_5.Name = "btn_Number_5";
+            this.btn_Number_5.Size = new System.Drawing.Size(109, 85);
+            this.btn_Number_5.TabIndex = 15;
+            this.btn_Number_5.Text = "5";
+            this.btn_Number_5.UseVisualStyleBackColor = true;
+            this.btn_Number_5.Click += new System.EventHandler(this.btn_Number_5_Click);
+            // 
+            // btn_Number_2
+            // 
+            this.btn_Number_2.Location = new System.Drawing.Point(151, 318);
+            this.btn_Number_2.Name = "btn_Number_2";
+            this.btn_Number_2.Size = new System.Drawing.Size(109, 85);
+            this.btn_Number_2.TabIndex = 14;
+            this.btn_Number_2.Text = "2";
+            this.btn_Number_2.UseVisualStyleBackColor = true;
+            this.btn_Number_2.Click += new System.EventHandler(this.btn_Number_2_Click);
+            // 
+            // btn_Number_9
+            // 
+            this.btn_Number_9.Location = new System.Drawing.Point(282, 500);
+            this.btn_Number_9.Name = "btn_Number_9";
+            this.btn_Number_9.Size = new System.Drawing.Size(109, 85);
+            this.btn_Number_9.TabIndex = 19;
+            this.btn_Number_9.Text = "9";
+            this.btn_Number_9.UseVisualStyleBackColor = true;
+            this.btn_Number_9.Click += new System.EventHandler(this.btn_Number_9_Click);
+            // 
+            // btn_Number_6
+            // 
+            this.btn_Number_6.Location = new System.Drawing.Point(282, 409);
+            this.btn_Number_6.Name = "btn_Number_6";
+            this.btn_Number_6.Size = new System.Drawing.Size(109, 85);
+            this.btn_Number_6.TabIndex = 18;
+            this.btn_Number_6.Text = "6";
+            this.btn_Number_6.UseVisualStyleBackColor = true;
+            this.btn_Number_6.Click += new System.EventHandler(this.btn_Number_6_Click);
+            // 
+            // btn_Number_3
+            // 
+            this.btn_Number_3.Location = new System.Drawing.Point(282, 318);
+            this.btn_Number_3.Name = "btn_Number_3";
+            this.btn_Number_3.Size = new System.Drawing.Size(109, 85);
+            this.btn_Number_3.TabIndex = 17;
+            this.btn_Number_3.Text = "3";
+            this.btn_Number_3.UseVisualStyleBackColor = true;
+            this.btn_Number_3.Click += new System.EventHandler(this.btn_Number_3_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(412, 409);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(109, 176);
+            this.btnDelete.TabIndex = 20;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btn_Number_0
+            // 
+            this.btn_Number_0.Location = new System.Drawing.Point(412, 318);
+            this.btn_Number_0.Name = "btn_Number_0";
+            this.btn_Number_0.Size = new System.Drawing.Size(109, 85);
+            this.btn_Number_0.TabIndex = 21;
+            this.btn_Number_0.Text = "0";
+            this.btn_Number_0.UseVisualStyleBackColor = true;
+            this.btn_Number_0.Click += new System.EventHandler(this.btn_Number_0_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 631);
+            this.Controls.Add(this.btn_Number_0);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btn_Number_9);
+            this.Controls.Add(this.btn_Number_6);
+            this.Controls.Add(this.btn_Number_3);
+            this.Controls.Add(this.btn_Number_8);
+            this.Controls.Add(this.btn_Number_5);
+            this.Controls.Add(this.btn_Number_2);
+            this.Controls.Add(this.btn_Number_7);
+            this.Controls.Add(this.btn_Number_4);
+            this.Controls.Add(this.btn_Number_1);
+            this.Controls.Add(this.Current_Date_Label);
+            this.Controls.Add(this.lbl_Current_Date);
+            this.Controls.Add(this.lbl_Date_Of_Birth);
+            this.Controls.Add(this.lbl_ID_Checker);
+            this.Controls.Add(this.txtDoB);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(563, 678);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(563, 678);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "ID_Checker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDoB;
+        private System.Windows.Forms.Label lbl_ID_Checker;
+        private System.Windows.Forms.Label lbl_Date_Of_Birth;
+        private System.Windows.Forms.Label lbl_Current_Date;
+        private System.Windows.Forms.Label Current_Date_Label;
+        private System.Windows.Forms.Button btn_Number_1;
+        private System.Windows.Forms.Button btn_Number_4;
+        private System.Windows.Forms.Button btn_Number_7;
+        private System.Windows.Forms.Button btn_Number_8;
+        private System.Windows.Forms.Button btn_Number_5;
+        private System.Windows.Forms.Button btn_Number_2;
+        private System.Windows.Forms.Button btn_Number_9;
+        private System.Windows.Forms.Button btn_Number_6;
+        private System.Windows.Forms.Button btn_Number_3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btn_Number_0;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ ID_Check/ID_Check/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ID_Check
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            Current_Date_Label.Text = DateTime.Now.ToShortDateString();
+            
+        }
+
+        private void btn_Number_1_Click(object sender, EventArgs e)
+        {
+
+            if (txtDoB.TextLength < 8)
+            {
+
+                txtDoB.Text = txtDoB.Text + "1";
+
+            }
+
+        }
+
+        private void btn_Number_2_Click(object sender, EventArgs e)
+        {
+
+            if (txtDoB.TextLength < 8)
+            {
+
+                txtDoB.Text = txtDoB.Text + "2";
+
+            }
+
+        }
+
+        private void btn_Number_3_Click(object sender, EventArgs e)
+        {
+
+            if (txtDoB.TextLength < 8)
+            {
+
+                txtDoB.Text = txtDoB.Text + "3";
+
+            }
+
+        }
+
+        private void btn_Number_4_Click(object sender, EventArgs e)
+        {
+
+            if (txtDoB.TextLength < 8)
+            {
+
+                txtDoB.Text = txtDoB.Text + "4";
+
+            }
+
+        }
+
+        private void btn_Number_5_Click(object sender, EventArgs e)
+        {
+
+            if (txtDoB.TextLength < 8)
+            {
+
+                txtDoB.Text = txtDoB.Text + "5";
+
+            }
+
+        }
+
+        private void btn_Number_6_Click(object sender, EventArgs e)
+        {
+
+           if (txtDoB.TextLength < 8)
+            {
+
+                txtDoB.Text = txtDoB.Text + "6";
+
+            }
+
+        }
+
+        private void btn_Number_7_Click(object sender, EventArgs e)
+        {
+
+            if (txtDoB.TextLength < 8)
+            {
+
+                txtDoB.Text = txtDoB.Text + "7";
+
+            }
+
+        }
+
+        private void btn_Number_8_Click(object sender, EventArgs e)
+        {
+
+            if (txtDoB.TextLength < 8)
+            {
+
+                txtDoB.Text = txtDoB.Text + "8";
+
+            }
+
+        }
+
+        private void btn_Number_9_Click(object sender, EventArgs e)
+        {
+
+            if (txtDoB.TextLength < 8)
+            {
+
+                txtDoB.Text = txtDoB.Text + "9";
+
+            }
+
+        }
+
+        private void btn_Number_0_Click(object sender, EventArgs e)
+        {
+
+            if (txtDoB.TextLength < 8)
+            {
+
+                txtDoB.Text = txtDoB.Text + "0";
+
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            if (txtDoB.TextLength > 0)
+            {
+
+                txtDoB.Text = txtDoB.Text.Substring(0, txtDoB.TextLength - 1);
+
+            }
+             
+        }
+
+        private 
+    }
+}
